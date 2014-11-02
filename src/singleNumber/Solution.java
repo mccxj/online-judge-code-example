13bbf553c49b2b52d9a9c7bb78dadc2d0464c37f
xiaojian cai <mc02cxj@gmail.com>
2014-11-02 15:11:11 +0800

    https://oj.leetcode.com/problems/single-number/

---
+++ src/singleNumber/Solution.java
@@ -0,0 +1,29 @@
+package singleNumber;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * <p/>
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
+public class Solution {
+    public int singleNumber(int[] A) {
+        int n = 0;
+        for (int e : A) {
+            n ^= e;
+        }
+        return n;
+    }
+
+    @Test
+    public void test() {
+        Solution s = new Solution();
+        assertEquals(2, s.singleNumber(new int[]{1, 2, 3, 1, 3}));
+        assertEquals(1, s.singleNumber(new int[]{1, 3, 21, 21, 3}));
+        assertEquals(-2, s.singleNumber(new int[]{1, 0, -1, 21, 21, -1, -2, 0, 1}));
+    }
+}
