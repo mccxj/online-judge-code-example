0a64512e1092f6714fcb60fcb8438716c2c43894
xiaojian cai <mc02cxj@gmail.com>
2017-01-14 15:19:27 +0800

    Update code_helper.py

--- projecteuler/code_helper.py
+++ projecteuler/code_helper.py
@@ -1,3 +1,6 @@
+from bisect import bisect_left
+
+
 def reverse_number(n):
     s = 0
     while n != 0:
@@ -45,3 +48,10 @@ def is_prime(n):
         if i * i > n:
             return True
     return True
+
+
+def binary_search(a, x, lo=0,
+                  hi=None):  # can't use a to specify default for hi
+    hi = hi if hi is not None else len(a)  # hi defaults to len(a)   
+    pos = bisect_left(a, x, lo, hi)  # find insertion position
+    return (pos if pos != hi and a[pos] == x else -1)  # don't walk off the end
